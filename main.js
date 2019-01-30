109be9a4e9261cfbf45e5c5d41c279da8595fbb4
Unknown <shani.rubovitch@gmail.com>
2019-01-30 15:32:34 +0100

    function codePhrase added

---
+++ main.js
@@ -0,0 +1,29 @@
+// solving without strings, only using ascii codes as int?
+
+const ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+function codePhrase(phrase) {
+
+    let upcasePhrase = phrase.toUpperCase()
+    let codedPhrase = ""
+
+    for (let i = 0; i < upcasePhrase.length(); i++) {
+        codedPhrase += codeChar(upcasePhrase.indexOf(i))
+    }
+
+    console.error("the original phrase: " + phrase + ". the coded phrase: " + codedPhrase)
+}
+
+function codeChar(letter) {
+
+    letterIndex = ABC.indexOf(letter)
+    if (letterIndex == -1) {
+        return letter
+    }
+
+    returnIndex = index + 13
+    if (returnIndex > ABC.length)
+        returnIndex -= ABC.length
+
+    return ABC.charAt(returnIndex)
+}
